4af44c50da0a29478b1909f8be57d3387353cca6
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-10 22:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function Scooter(year, color, model) {
-  this.year = 2015
-  this.age = "red"
-  this.model = "swoosh"
+  this.year = year
+  this.age = color
+  this.model = model
 }
+
+let samson = new Scooter(2015, "red", "swoosh")
